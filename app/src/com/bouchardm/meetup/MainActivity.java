8a8da37218e2bd5802c3323b4e4ffebca972a00b
Francis-Ouellet <franc.ouelle93@gmail.com>
2014-02-23 16:33:59 -0500

    Connexion via G+

--- app/src/com/bouchardm/meetup/MainActivity.java
+++ app/src/com/bouchardm/meetup/MainActivity.java
@@ -1,22 +1,85 @@
 package com.bouchardm.meetup;
 
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 
+import com.bouchardm.meetup.menuUtil.*;
+
+public class MainActivity extends FragmentActivity  {
+
+	private static final int LOGIN_REQUEST = 0;
+	private GoogleApiClient mGoogleApiClient;
+	
+	private DrawerLayout mDrawer;
+	private ListView mLeftDrawerList;
+	private String[] mLeftMenuItems;
+	//private CustomActionBarDrawerToggle mLeftDrawerToggle;
+	
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		Intent connection = new Intent(this,ConnectionActivity.class);
+		this.startActivity(connection);
+		
+		LoggedPerson user = ((LoggedPerson)getApplicationContext());
+		mGoogleApiClient = user.getClient();
+		
+		mDrawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+		
+		initLeftMenu();
+		
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onStart() {
+		super.onStart();
 	}
 
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
+	
+	private void initLeftMenu(){
+		NsMenuAdapter mAdapter = new NsMenuAdapter(this);
+		
+		
+		
+	}
+	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView parent, View view, int position,long id) {
+
+			// Highlight the selected item, update the title, and close the drawer
+			// update selected item and title, then close the drawer
+			mDrawerList.setItemChecked(position, true);
+			setTitle("......");
+
+			String text= "menu click... should be implemented";
+			//Toast.makeText(MainActivity.this, text , Toast.LENGTH_LONG).show();
+			mDrawer.closeDrawer(mDrawerList);
+
+		}
+	}
 }
+
